c8500446aca3d3dab47fe5b41de8f27ff36c6e81
FabioClem <fabio.henrique.clem@gmail.com>
2019-08-19 15:30:44 -0300

    correção ws adição ws.

--- lib/app.ts
+++ lib/app.ts
@@ -3,16 +3,22 @@ import * as bodyParser from "body-parser";
 import { Routes } from "./routes/crmRoutes";
 import * as mongoose from "mongoose";
 
+import * as WebSocket from 'ws';
+
+import * as http from 'http';
+
 class App {
 
     public app: express.Application = express();
     public routePrv: Routes = new Routes();
-    // public mongoUrl: string = 'mongodb://localhost/CRMdb';  
     public mongoUrl: string = 'mongodb://localhost:27017/CRMdb';
+
+    public serv = http.createServer(this.app);
 
     constructor() {
         this.config();
         this.mongoSetup();
+        this.wsConfig();
         this.routePrv.routes(this.app);
     }
 
@@ -28,6 +34,24 @@ class App {
         mongoose.connect(this.mongoUrl, { useNewUrlParser: true, useFindAndModify: false });
     }
 
+    public wsConfig() {
+
+        const wss = new WebSocket.Server({ port: 3001 });
+
+        wss.on('connection', (ws: WebSocket) => {
+
+            //connection is up, let's add a simple simple event
+            ws.on('message', (message: string) => {
+
+                //log the received message and send it back to the client
+                console.log('received: %s', message);
+                ws.send(`Hello, you sent -> ${message}`);
+            });
+
+            //send immediatly a feedback to the incoming connection    
+            ws.send('Hi there, I am a WebSocket server');
+        });
+    }
 }
 
 export default new App().app;
--- lib/server.ts
+++ lib/server.ts
@@ -1,8 +1,8 @@
-
 import app from './app';
-import * as https from 'https';
+// import * as https from 'https';
+import * as http from 'http';
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
-})
+});
